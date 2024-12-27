04e78bc13f377a26c3fb7cab80b95b2b943c65a6
Serpej <Jehag@ive.se>
2024-12-27 12:37:40 +0100

    Add a target to the display and some comments on subproblems to solve next

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ let subtract = (leftNum, rightNum) => leftNum - rightNum;
 let multiply = (leftNum, rightNum) => leftNum * rightNum;
 let divide = (leftNum, rightNum) => leftNum / rightNum;
 
+const display = document.querySelector(".display");
+
+// Target alla buttons (för sig? eller alla har samma något slags bubblande?)
+// Koppla event listeners för klick på knappar till att skapa något på "display" (div med innehåll?).
+// Koppla event listeners för knapptryckningar för samma sak.
+// vid tryck på "=" ska operate funktionen användas.
+// vid "clear" ska allt på displayen tas bort.
+
 let test = multiply(30, 30);
 console.log(test);
 
