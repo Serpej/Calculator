dd44ec892c392e1820e14bb4f3b570b1c63e9e64
Serpej <Jehag@ive.se>
2024-12-27 10:39:22 +0100

    Add arrow functions for +, -, *, /

---
+++ script.js
@@ -0,0 +1,4 @@
+let add = (leftNum, rightNum) => leftNum + rightNum;
+let subtract = (leftNum, rightNum) => leftNum - rightNum;
+let multiply = (leftNum, rightNum) => leftNum * rightNum;
+let divide = (leftNum, rightNum) => leftNum / rightNum;
