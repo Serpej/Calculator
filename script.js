67cfa4fc21c50fe81b2931b56a92965941f88618
Serpej <Jehag@ive.se>
2024-12-27 10:50:47 +0100

    Add function operate to call math functions

--- script.js
+++ script.js
@@ -2,3 +2,25 @@ let add = (leftNum, rightNum) => leftNum + rightNum;
 let subtract = (leftNum, rightNum) => leftNum - rightNum;
 let multiply = (leftNum, rightNum) => leftNum * rightNum;
 let divide = (leftNum, rightNum) => leftNum / rightNum;
+
+let test = multiply(30, 30);
+console.log(test);
+
+let leftNum;
+let Operator;
+let rightNum;
+
+function operate(leftNum, operator, rightNum) {
+    if (operator = "+") {
+        return add(leftNum, rightNum);
+
+    } else if (operator = "-") {
+        return subtract(leftNum, rightNum);
+
+    } else if (operator = "*") {
+        return multiply(leftNum,rightNum);
+        
+    } else if (operator = "/") {
+        return divide(leftNum,rightNum);
+    }
+}
